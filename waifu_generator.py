042aeb30703b982f6bd0c65e2cb093a25e5cd4c3
15Floater <85901236+15Floater@users.noreply.github.com>
2021-09-17 21:09:38 -0400

    Improved aesthetics.
    Fixed up spacing, colors, and fonts.

--- waifu_generator.py
+++ waifu_generator.py
@@ -8,26 +8,32 @@ from resizeimage import resizeimage
 
 
 
-#api
+
 def api_call():
+    '''Calls the Waifu API, converts link to an image, resizes the image,
+    and then updates the label in Tkinter with the new image'''
     global im
     endpoint = f'https://waifu.pics/api/sfw/{clicked.get()}'
 
 
-
+    #the actual API call
     response = requests.get(endpoint)
     response.raise_for_status()
     data = response.json()
     global url
     url = data['url']
-    print(url)
 
+    #takes the image, opens it up, gets width and height (for later)
     image = Image.open(requests.get(url, stream=True).raw)
-    image = resizeimage.resize_contain(image, [200, 300], bg_color=(255, 255, 255, 0))
+    width, height = image.size
+    print(width, height)
 
+    #resizes the image and changes the background color
+    operation1 = resizeimage.resize_width(image, 300)
+    image = resizeimage.resize_contain(operation1, [450, 400], bg_color=(255, 250, 205))
+
+    #converts the image into an ImageTk so it can be configured onto the label
     im = ImageTk.PhotoImage(image)
-    
-
     starter_label.config(image=im)
 
 
@@ -38,23 +44,23 @@ def api_call():
     
 
 
-#window creation
+#creates window, adds title, and padding/bg color
 window = Tk()
 window.title('Waifu Generator')
+window.config(padx=20, pady=20, bg='#FFFACD')
 
 #label creation
-welcome_label = Label(text='Welcome to the Waifu Generator!')
-welcome_label.grid(row=0, column=0, columnspan=3)
+welcome_label = Label(text='Welcome to the Waifu Generator!', bg='#FFFACD',
+font=('Arial', 15, 'bold'))
+welcome_label.grid(row=0, column=0, columnspan=2)
 
-choose_label = Label(text='さあ。。。ワイフを選んで下さい♡')
-choose_label.grid(row=1, column=0, columnspan=3)
-
-starter_label = Label(text='Here\'s Nino (best girl)!')
-starter_label.grid(row=2, column=0)
+choose_label = Label(text='さあ。。。ワイフを選んで下さい♡', bg='#FFFACD',
+font=('Arial', 12, 'italic'))
+choose_label.grid(row=1, column=0, columnspan=2)
 
 #button creation
 finalize_button = Button(text='Finalize choice', command=api_call)
-finalize_button.grid(row=4, column=1)
+finalize_button.grid(row=3, column=1)
 
 
 #dropdown creation
@@ -64,15 +70,18 @@ options=['waifu', 'neko', 'megumin', 'bully', 'cuddle',
 clicked = StringVar()
 clicked.set('Type Selector')
 
+#dropdown command + feis
 drop = OptionMenu(window, clicked, *options)
-drop.grid(row=2, column=2)
+drop.grid(row=3, column=0)
 
 #initial image
+#or at least that's what it was supposed to be
+#now it gets the initial image, but starter_image is also what gets updated
 nino_filepath = 'OneDrive/Desktop/Personal/waifu_generator/images/nino_template2.jpg'
 starter_image = ImageTk.PhotoImage(Image.open(nino_filepath))
 starter_label = Label(window, image=starter_image)
-#starter_label.config(bg='#Fdfcfa', highlightthickness=0)
-starter_label.grid(row=2, column=1)
+starter_label.config(bg='#FFFACD', highlightthickness=0)
+starter_label.grid(row=2, column=0, columnspan=2)
 
 
 
